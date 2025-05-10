dae92f5b3efafff98e1e95e08595177d48a3c6bc
chiragvala00789 <valachirag00789@gmail.com>
2025-05-10 16:34:17 +0000

    datatype summery

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountNAme = "hitesh"
+var accountPassword = "259968"
+accountCity = "Surat"
+let accountState;
+// accountId = 598674; not allowed for this 
+accountNAme = "chirag"
+accountPassword = "5869547"
+accountCity = "Mumbai"
+console.log(accountNAme);
+console.table([accountId,accountNAme,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict";
+// alert("Hello") we are use node js not browser
+console.log(3+3);
+console.log("chirag"); //code readability shoud be high
+
+let name = "Chirag"
+let age = 25
+let isloggedIn = true;
+
+// number 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
